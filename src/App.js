b744da376bdfacaedece03252bc906994d6962ff
Shaurya Vardhan <shaurya.vardhan.9758@gmail.com>
2023-01-26 22:36:22 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-
 import './App.css';
+import {useGetLimitPostQuery} from './services/posts';
+
 
 function App() {
+  /*const responseInfo = useGetAllPostQuery()*/
+  // const responseInfo = useGetPostByIdQuery(5);
+  const responseInfo = useGetLimitPostQuery(10);
+  console.log(responseInfo.data);
   return (
-    <div className="App">
+    <div >
+      {
+        responseInfo.data.map((post,i) => 
+        <div key={i}>
+          <h2>{post.id} :- {post.title}</h2>
+          <p>  {post.body}</p>
+          <hr />
+        </div>)
+      }
     </div>
   );
 }
--- src/services/posts.js
+++ src/services/posts.js
@@ -5,14 +5,33 @@ export const postApi = createApi({
     baseQuery:fetchBaseQuery({
         baseUrl:"https://jsonplaceholder.typicode.com/"
     }),
-    endpoints:(builder)=>{
+    endpoints: (builder) => ({
         getAllPost: builder.query({
-            query:()=>({
-                url:"posts",
-                method:"GET"
-            })
-        })
-    }
+         query: () => ({
+          url: 'posts',
+          method: 'GET'
+         })
+        }),
+        getPostById: builder.query({
+            query: (id) => {
+             console.log("ID:", id)
+             return {
+              url: `posts/${id}`,
+              method: 'GET'
+             }
+            }
+           }),
+        getLimitPost: builder.query({
+            query: (num) => {
+             console.log("Limit Number:", num)
+             return {
+              url: `posts?_limit=${num}`,
+              method: 'GET'
+             }
+            }
+           })
+    })
 })
 
-export const {useGetAllPostQuery} = postApi;
+export const {useGetAllPostQuery, useGetPostByIdQuery, useGetLimitPostQuery} = postApi
+// export const { useGetPostByIdQuery} = postApi
